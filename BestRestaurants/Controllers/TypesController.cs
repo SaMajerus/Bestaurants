60924ff609d25ee7496d7122e7ae7f54f495aa24
Skylan Lew <skylanslew@gmail.com>
2022-10-05 15:09:54 -0700

    RESTful routing Types
    Co-authored-by: Skylan Lew <skylanslew@gmail.com>
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>

--- BestRestaurants/Controllers/TypesController.cs
+++ BestRestaurants/Controllers/TypesController.cs
@@ -19,5 +19,53 @@ namespace BestRestaurants.Controllers
       List<Restaurant> model = _db.Restaurants.Include(restaurant => restaurant.Type).ToList();
       return View(model); 
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Type type)
+    {
+      _db.Types.Add(type);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Show(int id)
+    {
+      Type thisType = _db.Types.FirstOrDefault(type => type.Type == id);
+      return View(thisType);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Type thisType = _db.Types.FirstOrDefault(type => type.TypeId == id);
+      return View(thisType);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Type type)
+    {
+      _db.Entry(type).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Type thisType = _db.Types.FirstOrDefault(type => type.TypeId == id);
+      return View(thisType);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Type thisType = _db.Types.FirstOrDefault(type => type.TypeId == id);
+      _db.Types.Remove(thisType);
+      _db.SavesChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
