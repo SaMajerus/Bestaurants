fb2a22a6c59d0edce7a6c20a78159ee2529fd853
Skylan Lew <skylanslew@gmail.com>
2022-10-06 16:44:36 -0700

    add readme, databasefile
    Co-authored-by: Keisha Hepner <kmhepner90@gmail.com>
    Co-authored-by: Samuel Majerus <ladolego@gmail.com>

--- best_restaurants.sql
+++ best_restaurants.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `cuisines`
+--
+
+DROP TABLE IF EXISTS `cuisines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuisines` (
+  `CuisineId` int NOT NULL AUTO_INCREMENT,
+  `CuisineName` varchar(255) NOT NULL,
+  PRIMARY KEY (`CuisineId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuisines`
+--
+
+LOCK TABLES `cuisines` WRITE;
+/*!40000 ALTER TABLE `cuisines` DISABLE KEYS */;
+INSERT INTO `cuisines` VALUES (1,'Chinese'),(2,'Thai'),(3,'American'),(4,'German'),(5,'English'),(6,'Japanese'),(7,'Mongolian'),(8,'Ghanaean'),(9,'Indian'),(10,'Mexian'),(11,'Tex-Mex'),(12,'Soylent');
+/*!40000 ALTER TABLE `cuisines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `restaurants`
 --
 
@@ -28,9 +52,9 @@ CREATE TABLE `restaurants` (
   `Rating` int DEFAULT '3',
   `Cost` int DEFAULT '2',
   `Review` varchar(255) DEFAULT NULL,
-  `TypeId` int NOT NULL DEFAULT '0',
+  `CuisineId` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`RestaurantId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,32 +63,8 @@ CREATE TABLE `restaurants` (
 
 LOCK TABLES `restaurants` WRITE;
 /*!40000 ALTER TABLE `restaurants` DISABLE KEYS */;
-INSERT INTO `restaurants` VALUES (3,'amazon.com',1,1,'He\'s watching me',5);
+INSERT INTO `restaurants` VALUES (1,'asdf',1,1,NULL,1),(2,'asdf',1,1,NULL,1),(4,'asdf',1,1,NULL,1),(5,'234232',2,2,'12415125',1),(6,'spencer\'s house of hoisin',5,3,NULL,1),(7,'asdf',1,1,NULL,1),(8,'asfdsss',1,1,NULL,5),(9,'asdf',1,1,NULL,1),(10,'123123',1,1,NULL,1),(11,'asdf',1,1,NULL,1),(12,'asdfasd',1,1,NULL,1);
 /*!40000 ALTER TABLE `restaurants` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `types`
---
-
-DROP TABLE IF EXISTS `types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `types` (
-  `TypeId` int NOT NULL AUTO_INCREMENT,
-  `TypeName` varchar(255) NOT NULL,
-  PRIMARY KEY (`TypeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `types`
---
-
-LOCK TABLES `types` WRITE;
-/*!40000 ALTER TABLE `types` DISABLE KEYS */;
-INSERT INTO `types` VALUES (1,'Chinese'),(2,'Thai'),(3,'American'),(4,'German'),(5,'English'),(6,'Ghanaean'),(7,'Mongolian');
-/*!40000 ALTER TABLE `types` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -76,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-06 14:16:11
+-- Dump completed on 2022-10-06 16:42:29
